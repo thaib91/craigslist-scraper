776dd64feb7717a98d5b60504fd59b1fbab82544
Thai Bui <tbui64@gmail.com>
2019-03-13 17:08:42 -0400

    Updated dummy data to emulate jquery results

--- server/server.js
+++ server/server.js
@@ -10,15 +10,27 @@ const {SERVER_PORT} = process.env;
 //web scraping
 const request = require('request-promise'); //download web pages
 
-const cheerio = require('cheerio'); //select different elements
+const cheerio = require('cheerio'); //select different elements - similar to jquery (select by css selector)
+//use the developer tool to see what you're trying to find in regards to CSS
 // const rq = require('request');
+
+const scrapeResult = {
+    title: "Cozy home perfectly suited for you!",
+    description: "info about the apartment",
+    datePosted: new Date("2019-03-12"),
+    url: "https://denver.craigslist.org/apa/d/englewood-cozy-home-perfectly-suited/6833612198.html",
+    hood: '(Denver Tech Center / Centennial)',
+    address: '10200 E Dry Creek Rd',
+    cost: '$1647/month'
+}
 
 const url = "https://denver.craigslist.org/d/apts-housing-for-rent/search/apa"
 
-const scrapeCraigslist = async () => { //function to take in html data of webpage into terminal
+const scrapeCraigslist = async () => { //function to take in html data (into string) of webpage into terminal
     try{
         const htmlResult = await request.get(url); //like an axios call but getting data from a URL
-        console.log(htmlResult)
+        const $ = await cheerio.load(htmlResult);
+
     }catch (err){
         console.error(err)
     }
