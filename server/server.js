8783c116fc4b31b36f793cc4c39aa7866dc8f7fc
Thai Bui <tbui64@gmail.com>
2019-03-12 23:46:48 -0400

    required data from craigslist into CLI

--- server/server.js
+++ server/server.js
@@ -1,28 +1,34 @@
-const express = require('express');
-const app = express();
-require('dotenv').config();
+// const express = require('express');
+// const app = express();
+// require('dotenv').config();
 
-app.use(express.static(`${__dirname}/../build`));
-app.use(express.json());
+// app.use(express.static(`${__dirname}/../build`));
+// app.use(express.json());
 
 const {SERVER_PORT} = process.env;
 
 //web scraping
-const rp = require('request-promise');
-const cheerio = require('cheerio');
-const Table = require('cli-table');
+const request = require('request-promise'); //download web pages
 
-let users = [];
+const cheerio = require('cheerio'); //select different elements
+// const rq = require('request');
 
-const options = {
-    url:
+const url = "https://denver.craigslist.org/d/apts-housing-for-rent/search/apa"
+
+const scrapeCraigslist = async () => { //function to take in html data of webpage into terminal
+    try{
+        const htmlResult = await request.get(url); //like an axios call but getting data from a URL
+        console.log(htmlResult)
+    }catch (err){
+        console.error(err)
+    }
 }
 
+scrapeCraigslist()
 
 
 
 
 
 
-
-app.listen(SERVER_PORT, () => console.log(`IT'S OVER ${SERVER_PORT}`));
+// app.listen(SERVER_PORT, () => console.log(`IT'S OVER ${SERVER_PORT}`));
